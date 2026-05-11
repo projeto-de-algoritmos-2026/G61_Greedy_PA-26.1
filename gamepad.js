f079c04e302325324949baf3e4332df97f5aee01
caio-venancio <caio.venancio784@gmail.com>
2026-05-11 20:16:08 -0300

    feat: adicionar detecção do gamepad

--- gamepad.js
+++ gamepad.js
@@ -1,31 +1,32 @@
-const inputState = {};
+export const inputState = {};
 
-function press(key) {
+export function press(key) {
     inputState[key] = true;
     console.log(key + " pressionado");
 }
 
-function release(key) {
+export function release(key) {
     inputState[key] = false;
     console.log(key + " solto");
 }
 
-document.querySelectorAll("button").forEach(button => {
+export function initInputButtons() {
+    document.querySelectorAll("button").forEach(button => {
+        const key = button.dataset.key;
+        
+        if (!key) return;
 
-    const key = button.dataset.key;
+        button.addEventListener("touchstart", e => {
+            e.preventDefault();
+            press(key);
+        });
 
-    // TOUCH
-    button.addEventListener("touchstart", e => {
-        e.preventDefault();
-        press(key);
+        button.addEventListener("touchend", e => {
+            e.preventDefault();
+            release(key);
+        });
+
+        button.addEventListener("mousedown", () => press(key));
+        button.addEventListener("mouseup", () => release(key));
     });
-
-    button.addEventListener("touchend", e => {
-        e.preventDefault();
-        release(key);
-    });
-
-    // MOUSE
-    button.addEventListener("mousedown", () => press(key));
-    button.addEventListener("mouseup", () => release(key));
-});
+}
